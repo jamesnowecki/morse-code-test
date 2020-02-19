f85957539bf0fd65da7ef68035d4b3b427012afb
James Nowecki <James.Nowecki@Nology.local>
2020-02-19 17:27:28 +0000

    fixed the issues and MC displays in HTML!

--- main.js
+++ main.js
@@ -1,81 +1,76 @@
 const morseCode = {
-    a: '._***',
-    b: '_...***',
-    c: '_._.***',
-    d: '_..***',
-    e: '.***',
-    f: '.._.***',
-    g: '__.***',
-    h: '....***',
-    i: '..***',
-    j: '.___***',
-    k: '_._***',
-    l: '._..***',
-    m: '__***',
-    n: '_.***',
-    o: '___***',
-    p: '.__.***',
-    q: '__._***',
-    r: '._.***',
-    s: '...***',
-    t: '_***',
-    u: '.._***',
-    v: '..._***',
-    w: '.__***',
-    x: '_.._***',
-    y: '_.__***',
-    z: '__..***',
-    0: '_____***',
-    1: '.____***',
-    2: '..___***',
-    3: '...__***',
-    4: '...._***',
-    5: '.....***',
-    6: '_....***',
-    7: '__...***',
-    8: '___..***',
-    9: '____.***',
-    ' ': '****'
+    a: "._***",
+    b: "_...***",
+    c: "_._.***",
+    d: "_..***",
+    e: ".***",
+    f: ".._.***",
+    g: "__.***",
+    h: "....***",
+    i: "..***",
+    j: ".___***",
+    k: "_._***",
+    l: "._..***",
+    m: "__***",
+    n: "_.***",
+    o: "___***",
+    p: ".__.***",
+    q: "__._***",
+    r: "._.***",
+    s: "...***",
+    t: "_***",
+    u: ".._***",
+    v: "..._***",
+    w: ".__***",
+    x: "_.._***",
+    y: "_.__***",
+    z: "__..***",
+    0: "_____***",
+    1: ".____***",
+    2: "..___***",
+    3: "...__***",
+    4: "...._***",
+    5: ".....***",
+    6: "_....***",
+    7: "__...***",
+    8: "___..***",
+    9: "____.***",
+    " ": "****"
 };
 
-const turnToLowerCase = (theInitialText) => {
+const turnToLowerCase = theInitialText => {
     return theInitialText.toLowerCase();
-}
+};
 
-const splitString = (stringIn) => {
-    return stringIn.split('');
-}
+const splitString = stringIn => {
+    return stringIn.split("");
+};
 
 const translateToMorse = (arrayCharacter) => {
     return morseCode[arrayCharacter];
 }
 
-const combineTheMorseArray = (theMorseArray) => {
+const combineTheMorseArray = theMorseArray => {
     return theMorseArray.toString();
-}
+};
 
-const removeComma = (theLongString) => {
-    return theLongString.replace(/,/g, '');
-}
+const removeComma = theLongString => {
+    return theLongString.replace(/,/g, "");
+};
 
-const convertToSpaces = (theStarredString) => {
-    return theStarredString.replace(/\*/g, ' ');
-}
-
+const convertToSpaces = theStarredString => {
+    return theStarredString.replace(/\*/g, " ");
+};
 
 const getsTheMessage = () => {
-
     let message = document.getElementById("message").value;
     let lowerCaseMessage = turnToLowerCase(message);
     let preMorseArray = splitString(lowerCaseMessage);
-    let postMorseArray = translateToMorse(preMorseArray);
-    let combinedStringWithCommas = comebineTheMorseArray(postMorseArray);
+    let postMorseArray = preMorseArray.map(translateToMorse); //works up to and inc here
+    let combinedStringWithCommas = combineTheMorseArray(postMorseArray);
     let combinedStringWithoutCommas = removeComma(combinedStringWithCommas);
-    let stringWithS = convertToSpaces(combinedStringWithoutCommas);
-
+    let stringWithSpaces = convertToSpaces(combinedStringWithoutCommas);
 
     let returnedMessage = document.getElementById("returnedMessage");
-    return returnedMessage.innerHTML = lowerCaseMessage;
-
-
-}
+    return (returnedMessage.innerHTML = stringWithSpaces);
+};
