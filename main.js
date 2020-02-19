ba6532ecc76be45aa05a872e372ba4e6b6915db4
James Nowecki <James.Nowecki@Nology.local>
2020-02-19 14:18:19 +0000

    completed morsing added to main code

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 const morseCode = {
-    a: '._',
-    b: '_...',
-    c: '_._.',
-    d: '_..',
-    e: '.',
-    f: '.._.',
-    g: '__.',
-    h: '....',
-    i: '..',
-    j: '.___',
-    k: '_._',
-    l: '._..',
-    m: '__',
-    n: '_.',
-    o: '___',
-    p: '.__.',
-    q: '__._',
-    r: '._.',
-    s: '...',
-    t: '_',
-    u: '.._',
-    v: '..._',
-    w: '.__',
-    x: '_.._',
-    y: '_.__',
-    z: '__..',
-    0: '_____',
-    1: '.____',
-    2: '..___',
-    3: '...__',
-    4: '...._',
-    5: '.....',
-    6: '_....',
-    7: '__...',
-    8: '___..',
-    9: '____.',
-    ' ': '    '
+    a: '._***',
+    b: '_...***',
+    c: '_._.***',
+    d: '_..***',
+    e: '.***',
+    f: '.._.***',
+    g: '__.***',
+    h: '....***',
+    i: '..***',
+    j: '.___***',
+    k: '_._***',
+    l: '._..***',
+    m: '__***',
+    n: '_.***',
+    o: '___***',
+    p: '.__.***',
+    q: '__._***',
+    r: '._.***',
+    s: '...***',
+    t: '_***',
+    u: '.._***',
+    v: '..._***',
+    w: '.__***',
+    x: '_.._***',
+    y: '_.__***',
+    z: '__..***',
+    0: '_____***',
+    1: '.____***',
+    2: '..___***',
+    3: '...__***',
+    4: '...._***',
+    5: '.....***',
+    6: '_....***',
+    7: '__...***',
+    8: '___..***',
+    9: '____.***',
+    ' ': '****'
 };
 
-const turnToLowerCase = (theInitialText) => { 
+const turnToLowerCase = (theInitialText) => {
     return theInitialText.toLowerCase();
 }
 
@@ -50,7 +50,9 @@ const translateToMorse = (arrayCharacter) => {
     return morseCode[arrayCharacter];
 }
 
-
+const combineTheMorseArray = (theMorseArray) => {
+    return theMorseArray.toString();
+}
 
 
 const getsTheMessage = () => {
@@ -59,14 +61,12 @@ const getsTheMessage = () => {
     let lowerCaseMessage = turnToLowerCase(message);
     let preMorseArray = splitString(lowerCaseMessage);
     let postMorseArray = translateToMorse(preMorseArray);
+    let combinedStringWithCommas = comebineTheMorseArray(postMorseArray);
 
 
 
-let returnedMessage = document.getElementById("returnedMessage");
-return returnedMessage.innerHTML = lowerCaseMessage;
+    let returnedMessage = document.getElementById("returnedMessage");
+    return returnedMessage.innerHTML = lowerCaseMessage;
 
 
 }
-
-
-
