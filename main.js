71d63d2f3c1d21e3b69625cc5577293605f9c14a
James Nowecki <James.Nowecki@Nology.local>
2020-02-27 16:10:41 +0000

    translate from morse to text works

--- main.js
+++ main.js
@@ -38,6 +38,47 @@ const morseCode = {
     " ": "****"
 };
 
+const morseCodeNoStars = {
+    a: "._",
+    b: "_...",
+    c: "_._.",
+    d: "_..",
+    e: ".",
+    f: ".._.",
+    g: "__.",
+    h: "....",
+    i: "..",
+    j: ".___",
+    k: "_._",
+    l: "._..",
+    m: "__",
+    n: "_.",
+    o: "___",
+    p: ".__.",
+    q: "__._",
+    r: "._.",
+    s: "...",
+    t: "_",
+    u: ".._",
+    v: "..._",
+    w: ".__",
+    x: "_.._",
+    y: "_.__",
+    z: "__..",
+    0: "_____",
+    1: ".____",
+    2: "..___",
+    3: "...__",
+    4: "...._",
+    5: ".....",
+    6: "_....",
+    7: "__...",
+    8: "___..",
+    9: "____.",
+    " ": " ",
+    "&nbsp": "/"
+};
+
 // Morse to text
 
 const turnToLowerCase = theInitialText => {
@@ -89,27 +130,30 @@ const splitMorseString = (morseString) => {
 }
 
 const getListOfLetters = morseChar => {
-    return translateToText(morseCode, morseChar)
+    return translateToText(morseChar)
+
 }
-const translateToText = (morseObject, morseChar) => {
-    return Object.keys(morseObject).find(key => morseObject[key] === morseChar);
-}
-
-
-
-const slashToSpaces = (longString) => {
-    return longString.replace(/\//g, "&nbsp");
+const translateToText = (morseChar) => {
+    return Object.keys(morseCodeNoStars).find(key => morseCodeNoStars[key] === morseChar);
 }
 
+// const slashToSpaces = (longString) => {
+//     return longString.replace(/\//g, "&nbsp");
+// }
 
 const getsMorseMessage = () => {
     const morseMessage = document.getElementById("morse-message").value;
+    console.log(morseMessage);
     const arrayOfMorseChars = splitMorseString(morseMessage);
+    console.log(arrayOfMorseChars);
     const postTextArray = arrayOfMorseChars.map(getListOfLetters);
+    console.log(postTextArray);
     const combinedTextStringWithCommas = combineTheMorseArray(postTextArray);
+    console.log(combinedTextStringWithCommas);
     const longStringText = removeComma(combinedTextStringWithCommas);
-    const finalTextString = slashToSpaces(longStringText);
+    console.log(longStringText)
+    // const finalTextString = slashToSpaces(longStringText);
 
     const returnedMorseMessage = document.getElementById("returnedMorseMessage");
-    return (returnedMorseMessage.innerHTML = finalTextString);
+    return (returnedMorseMessage.innerHTML = longStringText);
 }
--- testbed.js
+++ testbed.js
@@ -36,61 +36,101 @@ const morseCode = {
     8: '___..***',
     9: '____.***',
     ' ': '****',
-    '/': " "
+};
+
+const morseCodeNoStars = {
+    a: "._",
+    b: "_...",
+    c: "_._.",
+    d: "_..",
+    e: ".",
+    f: ".._.",
+    g: "__.",
+    h: "....",
+    i: "..",
+    j: ".___",
+    k: "_._",
+    l: "._..",
+    m: "__",
+    n: "_.",
+    o: "___",
+    p: ".__.",
+    q: "__._",
+    r: "._.",
+    s: "...",
+    t: "_",
+    u: ".._",
+    v: "..._",
+    w: ".__",
+    x: "_.._",
+    y: "_.__",
+    z: "__..",
+    0: "_____",
+    1: ".____",
+    2: "..___",
+    3: "...__",
+    4: "...._",
+    5: ".....",
+    6: "_....",
+    7: "__...",
+    8: "___..",
+    9: "____.",
+    " ": " ",
+    "&nbsp&nbsp&nbsp": "/"
 };
 
 
-const lowerCaseIt = (theText) => {
-    return theText.toLowerCase();
-}
+// const lowerCaseIt = (theText) => {
+//     return theText.toLowerCase();
+// }
 
-let theWord = "james panter helped me loads";
+// let theWord = "james panter helped me loads";
 
 // console.log(lowerCaseIt(frog))
 
 
 
 
-const splitString = (stringIn) => {
-    return stringIn.split('');
-}
+// const splitString = (stringIn) => {
+//     return stringIn.split('');
+// }
 
 
 
-const translateToMorse = (arrayCharacter) => {
-    return morseCode[arrayCharacter];
-}
+// const translateToMorse = (arrayCharacter) => {
+//     return morseCode[arrayCharacter];
+// }
 
-let splitArray = splitString(theWord);
+// let splitArray = splitString(theWord);
 
-console.log(splitArray.map(translateToMorse));
+// console.log(splitArray.map(translateToMorse));
 
-let MorseArray = (splitArray.map(translateToMorse))
-
-
-const combineTheMorseArray = (theMorseArray) => {
-    return theMorseArray.toString();
-}
-
-console.log(combineTheMorseArray(MorseArray));
+// let MorseArray = (splitArray.map(translateToMorse))
 
 
+// const combineTheMorseArray = (theMorseArray) => {
+//     return theMorseArray.toString();
+// }
 
-const sillyString = ".___***,._***,__***,.***,...***,****,.__.***,._***,_.***,_***,.***,._.***,****,....***,.***,._..***,.__.***,.***,_..***,****,__***,.***,****,._..***,___***,._***,_..***,...***";
+// console.log(combineTheMorseArray(MorseArray));
 
-const removeComma = (theLongString) => {
-    return theLongString.replace(/,/g, '');
-};
 
-console.log(removeComma(sillyString));
 
-let starredString = (removeComma(sillyString));
+// const sillyString = ".___***,._***,__***,.***,...***,****,.__.***,._***,_.***,_***,.***,._.***,****,....***,.***,._..***,.__.***,.***,_..***,****,__***,.***,****,._..***,___***,._***,_..***,...***";
 
-const generateSpace = (theLongString) => {
-    return theLongString.replace(/\*/g, '&nbsp');
-};
+// const removeComma = (theLongString) => {
+//     return theLongString.replace(/,/g, '');
+// };
 
-console.log(generateSpace(starredString));
+// console.log(removeComma(sillyString));
+
+// let starredString = (removeComma(sillyString));
+
+// const generateSpace = (theLongString) => {
+//     return theLongString.replace(/\*/g, '&nbsp');
+// };
+
+// console.log(generateSpace(starredString));
 
 const testString = ".___ . .. ___ / .. .. ___ / .__ .. . "
 
@@ -98,4 +138,12 @@ const splitMorseString = (morseString) => {
     return morseString.split(" ");
 };
 
+const testChar = "_._."
+
 console.log(splitMorseString(testString))
+
+const translateToText = (morseChar) => {
+    return Object.keys(morseCodeNoStars).find(key => morseCodeNoStars[key] === morseChar);
+}
+
+console.log(translateToText(testChar));
